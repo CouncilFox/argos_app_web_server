f090ca87d11a23f8368abaa80af8657425cf0a45
Jorden Gershenson <jordeng55@gmail.com>
2023-07-31 15:48:03 -0500

    Add API Docs

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,9 +4,50 @@ import generateToken from '../utils/generateToken.js'
 import UserActivity from '../models/userActivity.js'
 
 
-// @desc   Auth user/set token
-// route  POST /api/users/auth
-// @access Public
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User related APIs
+ */
+
+/**
+ * @swagger
+ * /api/users/auth:
+ *   post:
+ *     summary: Authenticate user and get token
+ *     tags: [Users]
+ *     description: Authenticate a user with email and password and return a token
+ *     produces:
+ *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User authenticated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       401:
+ *         description: Invalid email or password
+ */
 const authUser = asyncHandler( async (req,res) => {
      
      const {email, password} = req.body;
@@ -28,9 +69,39 @@ const authUser = asyncHandler( async (req,res) => {
         }  
 });
 
-// @desc   Log user activity
-// route  POST /api/users/activity
-// @access Private
+/**
+ * @swagger
+ * /api/users/activity:
+ *   post:
+ *     summary: Log user activity
+ *     tags: [Users]
+ *     description: Log user activity with actionType and page information
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - jwtToken: []
+ *     parameters:
+ *       - name: actionType
+ *         description: Type of action performed
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: page
+ *         description: Page on which the action was performed
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       201:
+ *         description: User activity logged successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ *       400:
+ *         description: Invalid activity data
+ */
 const logUserActivity = asyncHandler( async (req,res) => {
     const { actionType, page } = req.body;
     const userId = req.user._id;  // Grab the user ID from req.user, populated by the protect middleware
@@ -53,9 +124,46 @@ const logUserActivity = asyncHandler( async (req,res) => {
 
 
 
-// @desc   Register a new user
-// route  POST /api/users
-// @access Public
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     description: Register a new user with name, email, and password
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: name
+ *         description: User's name
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: User's email
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password
+ *         in: formData
+ *         required: true
+ *         type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *       400:
+ *         description: User already exists or invalid user data
+ */
 const registerUser = asyncHandler( async (req,res) => {
     const {name, email, password} = req.body;
 
@@ -89,9 +197,24 @@ const registerUser = asyncHandler( async (req,res) => {
 });
 
 
-// @desc   Logout user
-// route  POST /api/users/logout
-// @access Public
+/**
+ * @swagger
+ * /api/users/logout:
+ *   post:
+ *     summary: Logout user
+ *     tags: [Users]
+ *     description: Logout a user by clearing the JWT cookie
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: User logged out successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             message:
+ *               type: string
+ */
 const logoutUser = asyncHandler( async (req,res) => {
      res.cookie('jwt', '', {
         httpOnly: true,
@@ -101,9 +224,30 @@ const logoutUser = asyncHandler( async (req,res) => {
     res.status(200).json({message: "User logged out"});
 });
 
-// @desc   Get user profile
-// route  GET /api/users/profile
-// @access Private
+/**
+ * @swagger
+ * /api/users/profile:
+ *   get:
+ *     summary: Get user profile
+ *     tags: [Users]
+ *     description: Get the profile of the authenticated user
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - jwtToken: []
+ *     responses:
+ *       200:
+ *         description: User profile fetched successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ */
 const getUserProfile = asyncHandler( async (req,res) => {
      const user = {
         _id: req.user._id,
@@ -115,9 +259,48 @@ const getUserProfile = asyncHandler( async (req,res) => {
 });
 
 
-// @desc   Update user profile
-// route  PUT /api/users/profile
-// @access Public
+/**
+ * @swagger
+ * /api/users/profile:
+ *   put:
+ *     summary: Update user profile
+ *     tags: [Users]
+ *     description: Update the profile of the authenticated user
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - jwtToken: []
+ *     parameters:
+ *       - name: name
+ *         description: User's name
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: email
+ *         description: User's email
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User's password (optional)
+ *         in: formData
+ *         required: false
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: User profile updated successfully
+ *         schema:
+ *           type: object
+ *           properties:
+ *             _id:
+ *               type: string
+ *             name:
+ *               type: string
+ *             email:
+ *               type: string
+ *       404:
+ *         description: User not found
+ */
 const updateUserProfile = asyncHandler( async (req,res) => {
      const user = await User.findById(req.user._id);
 
--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,48 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express';
+import  YAML from 'yamljs';
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
-const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
 import path from 'path';
+
+dotenv.config();
+
+
+
+const port = process.env.PORT || 5000;
 
 connectDB();
 
 const app = express();
+
+const swaggerDocument = YAML.load('./backend/swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes)
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(
-        __dirname, 'frontend/dist'
-    )));
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
 } else {
-    app.get("/", (req, res) => res.send('Server is ready'));
+  app.get("/", (req, res) => res.send('Server is ready'));
 }
-
-
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
-// - **POST /api/users** - Register a user
-// - **POST /api/users/auth** - Authenticate a user and get token
-// - **POST /api/userslogout** - Logout a user and clear cookie
-// - **GET /api/users/profile** - Get user profile
-// - **PUT /api/users/profile** - Update user profile
