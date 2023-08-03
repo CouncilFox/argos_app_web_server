0e0849002ee7a5d0d6a00ab12bf4eaeebec2d0cd
Jorden Gershenson <jordeng55@gmail.com>
2023-08-02 21:30:28 -0500

    add Hi John

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
             <LinkContainer to='/'>
-                <Navbar.Brand>Argos Academy</Navbar.Brand>
+                <Navbar.Brand>Argos Academy Hi John!</Navbar.Brand>
             </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
