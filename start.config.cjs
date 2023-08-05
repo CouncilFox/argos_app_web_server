2d810b40995b15dff1ed2c68e684e675cafb04ad
Jorden Gershenson <jordeng55@gmail.com>
2023-08-04 20:07:57 -0500

    fix4 to post deploy script

--- start.config.cjs
+++ start.config.cjs
@@ -44,7 +44,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/CouncilFox/argos_app_web_server.git',
       path: '/home/jorden/apps',
-      'post-deploy': 'cd frontend && npm install && npm run build && cd ../backend && npm install && pm2 reload /home/jorden/apps/argos_app_web_server/current/start.config.cjs --env production && pwd'
+      'post-deploy': 'cd current/frontend && npm install && npm run build && cd ../backend && npm install && pm2 reload ../start.config.cjs --env production && pwd'
 
 
 
