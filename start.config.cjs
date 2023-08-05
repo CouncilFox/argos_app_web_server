2f469a9f624ff5a02424e94af84aa294b5b67d28
Jorden Gershenson <jordeng55@gmail.com>
2023-08-04 21:53:02 -0500

    change to priv key

--- start.config.cjs
+++ start.config.cjs
@@ -45,7 +45,7 @@ module.exports = {
       repo: 'https://github.com/CouncilFox/argos_app_web_server.git',
       path: '/home/jorden/apps/argos_app_web_server',
       'post-deploy': 'cd frontend && npm install && npm run build && cd ../backend && npm install && cd .. && pm2 restart start.config.cjs --env production',
-      key: 'C:/Users/jorde/.ssh/id_rsa.pub'  // Add this line
+      key: 'C:/Users/jorde/.ssh/id_rsa'  // Add this line
 
 
 
